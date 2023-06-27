c9690be203cca8392971e4a9230c6d9677a27bca
akulich87 <amgrupp_project@mail.ru>
2023-06-27 21:00:24 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[4];
+int sumOddPositions = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = new Random().Next(-99,100);
+
+  if (i % 2 == 1 )
+  {
+    sumOddPositions += array[i];
+  }
+}
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях, равна: {sumOddPositions}");
